403b0e3d9f45863ff11b0013d5331a50655fbad7
Ilya Shamovsky <ilya.shamovsky@gmail.com>
2014-08-28 16:56:10 -0400

    Another typo. Quotes in secret_key file.

--- ngs/keygen.py
+++ ngs/keygen.py
@@ -4,4 +4,4 @@ chars = '1234567890-=!@#$%^&*()_+qwertyuiopasdfghjkl;zxcvbnm,./'
 
 def generate_secret_key(filepath):
     with open(filepath,  'w') as keyfile:
-        keyfile.write('SECRET_KEY = %s' % get_random_string(50, chars))
+        keyfile.write('SECRET_KEY = "%s"' % get_random_string(50, chars))
