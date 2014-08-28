b0770ca5d6d7e8ff2dfda31996d643bce6be0e5f
Ilya Shamovsky <ilya.shamovsky@gmail.com>
2014-08-28 18:31:25 -0400

    Fixed urls

--- ngs/urls.py
+++ ngs/urls.py
@@ -6,18 +6,18 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # AUTHENTICATION VIEWS
-    url(r'^login/$', auth_views.login, {'template_name': 'auth/login.html',},
+    url(r'^cuff/login/$', auth_views.login, {'template_name': 'auth/login.html',},
         name='login_view'),
-    url(r'^logout/$', auth_views.logout_then_login,
+    url(r'^cuff/logout/$', auth_views.logout_then_login,
         name='logout_then_login_view'),
-    url(r'^password_change/$', auth_views.password_change,
+    url(r'^cuff/password_change/$', auth_views.password_change,
         {'template_name': 'auth/password.html',}, name='password_view'),
-    url(r'password_change_done/$', auth_views.password_change_done,
+    url(r'^cuff/password_change_done/$', auth_views.password_change_done,
         {'template_name': 'auth/password_change_done.html',},
         name='password_done_view'),
         
     url(r'^cuff/', include('cuff.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^cuff/admin/', include(admin.site.urls)),
 )
 
 try:
